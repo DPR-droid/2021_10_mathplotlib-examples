7e35f58cb7c5e9c3fd88360efd8fcdb6b2b4800f
Ryan <DavidP.Ryan@agriculture.gov.ie>
2021-10-13 21:41:13 +0100

    Python review

---
+++ 2dplot.py
@@ -0,0 +1,18 @@
+########################################################################
+# 2dplot.py
+# Author David
+# matplotlib tutorial
+########################################################################
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+#plt.show()
+
+plt.plot([1,2,3,4],[1,4,9,16])
+plt.ylabel('some numbers')
+#plt.show()
+
+plt.plot([1,2,3,4],[1,4,9,16], 'b.')
+plt.ylabel('some numbers')
+plt.show()
